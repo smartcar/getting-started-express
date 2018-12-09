74164a096c32511eb03390fbe7e32ea512c3a354
Karthik Bhaskara <karthik.bhaskara@smartcar.com>
2018-12-08 17:43:31 -0800

    Add README and Starter Kit

---
+++ README.md
@@ -0,0 +1,28 @@
+This is the starter kit for Server-Side Rendering with Express and Smartcar.
+
+This kit contains a simple web application that displays car information using Smartcar's Node SDK and Express.
+
+## Instructions
+Before we get started, create an application on Smartcar's Developer Dashboard to get your API keys.
+
+**Note:** On the dashboard, you will want to set your `redirect_uri` as `http://localhost:8000/exchange`.
+
+Then, we can set these as environment variables -
+```bash
+$ export CLIENT_ID=<your-client-id>
+$ export CLIENT_SECRET=<your-client-secret>
+$ export REDIRECT_URI=<your-redirect-uri>
+```
+
+Make sure you have cloned this repo -
+```bash
+$ git clone https://github.com/smartcar/getting-started-express.git
+$ cd getting-started-express/app
+```
+To install the required dependencies and run this Express app -
+```bash
+$ npm install
+$ node index.js
+```
+
+Once your server is up and running, you can authenticate your vehicle at `http://localhost:8000/login` in your browser. In our current set up, we are using Smartcar's [test mode](https://smartcar.com), so you can log in with any username and password and you will see information of a simulated vehicle.
---
+++ app/index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const express = require('express');
+const exphbs = require('express-handlebars');
+const smartcar = require('smartcar');
+
+const app = express();
+app.engine(
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', '.hbs');
+const port = 8000;
+
+const client = new smartcar.AuthClient({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
+  scope: ['read_vehicle_info'],
+  testMode: true,
+});
+
+// global variable to save our accessToken
+let access;
+
+app.get('/login', function(req, res) {
+  const authUrl = client.getAuthUrl();
+
+  res.render('home', {
+    url: authUrl,
+  });
+});
+
+app.get('/exchange', function(req, res) {
+  const code = req.query.code;
+
+  return client.exchangeCode(code)
+    .then(function(_access) {
+      // in a production app you'll want to store this in some kind of persistent storage
+      access = _access;
+
+      res.redirect('/vehicle');
+    });
+});
+
+app.get('/vehicle', function(req, res) {
+  return smartcar.getVehicleIds(access.accessToken)
+    .then(function(data) {
+      // the list of vehicle ids
+      return data.vehicles;
+    })
+    .then(function(vehicleIds) {
+      // instantiate the first vehicle in the vehicle id list
+      const vehicle = new smartcar.Vehicle(vehicleIds[0], access.accessToken);
+
+      return vehicle.info();
+    })
+    .then(function(info) {
+      res.render('vehicle', {
+        info: info,
+      });
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
